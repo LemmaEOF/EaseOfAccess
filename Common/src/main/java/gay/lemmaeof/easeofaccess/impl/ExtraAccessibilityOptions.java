2f358975f2dc09563d86cd3c5a58115d98cc7128
Meredith Espinosa <goclonefilms@gmail.com>
2022-08-02 21:26:21 -0700

    add tooltip for reduce motion

--- Common/src/main/java/gay/lemmaeof/easeofaccess/impl/ExtraAccessibilityOptions.java
+++ Common/src/main/java/gay/lemmaeof/easeofaccess/impl/ExtraAccessibilityOptions.java
@@ -2,11 +2,15 @@ package gay.lemmaeof.easeofaccess.impl;
 
 import gay.lemmaeof.easeofaccess.api.ExtraAccessibility;
 import net.minecraft.client.CycleOption;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 
 public final class ExtraAccessibilityOptions {
 
+	private static final Component ACCESSIBILITY_TOOLTIP_REDUCE_MOTION = new TranslatableComponent("options.accessibility.reduce_motion.tooltip");
 	public static final CycleOption<Boolean> REDUCE_MOTION = CycleOption.createOnOff(
 			"options.accessibility.reduce_motion",
+			ACCESSIBILITY_TOOLTIP_REDUCE_MOTION,
 			options -> ExtraAccessibility.reduceMotion(),
 			(options, option, value) -> ExtraAccessibilityImpl.INSTANCE.setReduceMotion(value)
 	);
